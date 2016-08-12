5da20ff59b8014dae2f8bce2bc1ae8c32a996286
Luiz Paro <lg_paro@hotmail.com>
2016-08-12 16:25:15 -0300

    Adding JavaDoc

--- src/main/java/com/passwordmeter/formula/Formula.java
+++ src/main/java/com/passwordmeter/formula/Formula.java
@@ -2,9 +2,28 @@ package com.passwordmeter.formula;
 
 import com.passwordmeter.model.BonusResult;
 import com.passwordmeter.model.Password;
+import com.passwordmeter.model.StatusValidation;
 
+/**
+ * Interface responsible for defining a contract for all {@link Formula}s in the application.
+ * A {@link Formula} takes a {@link Password} object and calculates its bonus according to a
+ * criteria defined in each implementation, like sequential letters, sequential numbers, etc.
+ * 
+ * @author Luiz Guilherme Paro
+ */
 @FunctionalInterface
 public interface Formula {
 
+    /**
+     * Method that receives a {@link Password} and check its internal state to calculate
+     * the bonus and status. The return consists in a {@link BonusResult} object containing
+     * both informations.
+     * 
+     * @param password - The password that needs to be checked against the criteria
+     * @return a {@link BonusResult} containing the calculus result.
+     * 
+     * @see BonusResult
+     * @see StatusValidation
+     */
     public BonusResult calculateBonus(Password password);
 }
--- src/main/java/com/passwordmeter/model/BonusResult.java
+++ src/main/java/com/passwordmeter/model/BonusResult.java
@@ -2,19 +2,47 @@ package com.passwordmeter.model;
 
 import java.util.Objects;
 
+import com.passwordmeter.formula.Formula;
+
+/**
+ * Immutable class that encapsulates the attributes bonus and status
+ * returned in a {@link Formula}'s calculation. Their results are part
+ * of the total password strength.
+ * 
+ * @author Luiz Guilherme Paro
+ * 
+ * @see Formula
+ * @see StatusValidation
+ */
 public final class BonusResult {
     private final int bonus;
     private final StatusValidation status;
     
+    /**
+     * Constructor that receives both bonus and status from a calculus.
+     * 
+     * @param bonus - Quantity of bonus that will make part of total password strengths
+     * @param status - Status of the validation, if its failure, sufficient or exceptional
+     */
     public BonusResult(int bonus, StatusValidation status) {
         this.bonus = bonus;
         this.status = Objects.requireNonNull(status, "status cannot be null");
     }
 
+    /**
+     * Get the calculated bonus.
+     * 
+     * @return bonus
+     */
     public int getBonus() {
         return bonus;
     }
 
+    /**
+     * Get the validation status.
+     * 
+     * @return status
+     */
     public StatusValidation getStatus() {
         return status;
     }
